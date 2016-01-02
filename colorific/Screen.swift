67c935587f9f2c3de799515b2017d3e28e5b07a9
aparna krishnan <aparnalocked@gmail.com>
2016-01-02 12:22:33 +0400

    Basic Version

---
+++ colorific/Screen.swift
@@ -0,0 +1,13 @@
+//
+//  Screen.swift
+//  colorific
+//
+//  Created by Aparna Krishnan on 12/30/15.
+//  Copyright © 2015 Aparna. All rights reserved.
+//
+
+import Cocoa
+
+class Screen: UIViewController {
+
+}
